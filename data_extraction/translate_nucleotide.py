b2a09a94d95f17fdd863441564665b4c2bb68e7c
scwatts <s.watts2@student.unimelb.edu.au>
2018-06-12 12:37:53 +1000

    Translate nucleotide script

---
+++ data_extraction/translate_nucleotide.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+'''Translate nucleotide FASTA records'''
+import argparse
+import pathlib
+
+
+from Bio.Seq import Seq
+from Bio.SeqIO.FastaIO import SimpleFastaParser
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_fp', required=True, type=pathlib.Path,
+            help='Input nucleotide FASTA filepath')
+
+    args = parser.parse_args()
+    if not args.input_fp.exists():
+        parser.error('Input file %s does not exist' % args.input_fp)
+    return args
+
+
+def main():
+    # Read in data
+    args = get_arguments()
+    with args.input_fp.open('r') as fh:
+        descs, seqs = zip(*SimpleFastaParser(fh))
+    seq_obj_gen = (Seq(seq) for seq in seqs)
+
+    # Translate and write out
+    for desc, seq_obj in zip(descs, seq_obj_gen):
+        print('>', desc, sep='')
+        seq = seq_obj.translate(stop_symbol='')
+        for line in [seq[i:i+80] for i in range(0, len(seq), 80)]:
+            print(line)
+
+
+if __name__ == '__main__':
+    main()
